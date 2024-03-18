33f3f12c8a191c37bac17a100519e4da797b2840
TaesukJun <taesuk92@gmail.com>
2024-03-19 01:22:51 +0900

    update

--- background_setup.py
+++ background_setup.py
@@ -1,1 +1,12 @@
+
+
 pip install matplotlib
+
+# ModuleNotFoundError: No module named 'imageio'
+
+# No module named 'ipympl'
+
+# No module named 'skimage'
+# pip install scikit-image
+
+# o module named 'lmfit'
